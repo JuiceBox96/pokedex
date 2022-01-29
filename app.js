a33647bdc538fbe56ae0cbfcb06ab7809bf9a5b4
JuiceBox96 <75124951+JuiceBox96@users.noreply.github.com>
2022-01-29 15:41:40 +0000

    need to import assets from package in order to display images and reverted some changes back

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-
-
-
 const poke_container = document.getElementById ('poke-container');
-const pokemon_count = 150
+const pokemon_count = 150;
 const colors = {
 	fire: '#FDDFDF',
 	grass: '#DEFDE0',
