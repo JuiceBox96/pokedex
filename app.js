d9eb9dcb1b081289cdf0ea1ea7fdf0732961465f
JuiceBox96 <75124951+JuiceBox96@users.noreply.github.com>
2022-01-30 18:20:41 +0000

    displays more pokemon

--- app.js
+++ app.js
@@ -34,33 +34,41 @@ const getPokemon = async id => {
 	createPokemonCard(pokemon);
 };
 
-function createPokemonCard(pokemon) {
-	const pokemonEl = document.createElement('div');
-	pokemonEl.classList.add('pokemon');
-
-	const poke_types = pokemon.types.map(type => type.type.name);
-	const type = main_types.find(type => poke_types.indexOf(type) > -1);
-	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-	const color = colors[type];
-	
-	pokemonEl.style.backgroundColor = color;
-
-	const pokeInnerHTML = `
-        <div class="img-container">
-            <img src="https://pokeres.bastionbot.org/images/${pokemon}" alt="${name}" />
-        </div>
-        <div class="info">
-            <span class="number">#${pokemon.id
-							.toString()
-							.padStart(3, '0')}</span>
-            <h3 class="name">${name}</h3>
-            <small class="type">Type: <span>${type}</span></small>
-        </div>
-    `;
-
-	pokemonEl.innerHTML = pokeInnerHTML;
-
-	poke_container.appendChild(pokemonEl);
-}
+function createPokemonCard(pokemon){
+	const pokemonEl = document.createElement('div')
+	pokemonEl.classList.add('pokemon')
+  
+	let idSecondDigit = () => {
+	   if (pokemon.id <= 9) {
+		  return 0
+	   } else if (pokemon.id >= 100) {
+		  return ''
+	   } else {
+		  return ''
+	   }
+	}
+  
+	let idThirdDigit = () => {
+	   if (pokemon.id >= 100) {
+		  return ''
+	   } else {
+		  return 0
+	   }
+	}
+  
+	pokemonInnerHTML = `
+	<div class="img-container">
+	   <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${idThirdDigit()}${idSecondDigit()}${pokemon.id}.png" alt="">
+	</div>
+	<div class="info">
+	   <span class="number">#001</span>
+	   <h3 class="name">Bulbasaur</h3>
+	   <small class="type">Type: <span>Grass</span></small>
+	</div>`
+  
+	pokemonEl.innerHTML = pokemonInnerHTML
+  
+	poke_container.appendChild(pokemonEl)
+ }
 
 fetchPokemons()
